0264b9d175cb00dd0958cde6e6bf9f3ac2df65aa
qwaszxxyf <38461178+qwaszxxyf@users.noreply.github.com>
2022-09-04 18:08:14 +0800

    Update 疫情.py

--- 自动填表/疫情.py
+++ 自动填表/疫情.py
@@ -13,8 +13,7 @@ opener = urllib.request.build_opener(handler)
 with open(user_file,'r',encoding='utf8') as load_f:
     userinfo = json.load(load_f)
 params = urllib.parse.urlencode(userinfo)
-with opener.open('https://app.upc.edu.cn/uc/wap/login/check', data=bytes(params, 'utf-8')) as resp:
-    print(resp.read().decode('utf-8'))
+
 cookie.save(ignore_discard=True, ignore_expires=True)
 
 headers1 = ('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',)
